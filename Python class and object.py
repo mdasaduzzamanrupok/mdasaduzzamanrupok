31dda68811afe87805634128bacc4c80c0d9b439
MD ASADUZZAMAN <121396092+mdasaduzzamanrupok@users.noreply.github.com>
2023-04-24 22:57:25 +0600

    Python Class and object

---
+++ Python class and object.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Python class and object
+
+#class
+class Student:     
+    roll = ""
+    Gpa  = ""
+
+#object
+rahim = Student()
+rahim.roll = 101
+rahim.Gpa  = 3.71
+print(f"Roll : {rahim.roll}, Gpa : {rahim.Gpa}")
+
+karim = Student()
+karim.roll = 102
+karim.Gpa  = 3.78
+print(f"Roll : {karim.roll}, Gpa : {karim.Gpa}")
+
+rupok = Student()
+rupok.roll = 103
+rupok.Gpa  = 3.58
+print(f"Roll : {rupok.roll}, Gpa : {rupok.Gpa}")
+
+
+# In[ ]:
+
+
+
+
